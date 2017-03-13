11e0f60de9b9b9f515e153e8020e86bee3881098
Jonathan Zaehringer <Jonathan.zaehringer@gmail.com>
2017-03-13 09:41:28 +0100

    Add search for project and project example

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(username: 'jzaehrin', email: 'jonathan.zaehringer@gmail.com', password: 'totem', password_confirmation: 'totem', is_admin: true);
+
+Project.create(title: 'Hungry Students', short_description: 'Application de reserve au restaurant via mobile.', status: 0, deadline: '30.04.2017', tags: 'Ionic, Augular2, Ruby on Rails, Reservation, Restaurant, Etudiant', nbr_people: 3, user_id: 1);
+
+Project.create(title: 'Raidplanner', short_description: 'Raidplanner pour un jeu specifique', status: 1, deadline: '30.06.2016', tags: 'Laravel, Jeux-video, Organisation', nbr_people: 4, user_id: 1);
+
+Project.create(title: 'Runners', short_description: 'Gestion des transports pour Paleo', status: 0, deadline: '28.02.2017', tags: 'Ionic, Laravel, Paleo, Organisation, cours', nbr_people: 8, user_id: 1);
